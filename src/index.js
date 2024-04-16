4142247e7eaca3a8f30b1fc0e7142e12ffaa0383
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-04-16 09:26:30 -0500

    Changed some things in index.js, which will act as my main.jsx.

--- src/index.js
+++ src/index.js
@@ -12,7 +12,13 @@ const router = createBrowserRouter([
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+/*const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
@@ -20,7 +26,7 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

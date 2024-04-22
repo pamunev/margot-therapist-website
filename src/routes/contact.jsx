be9e69a3fe9428fcec4dcb2e481d2732399944fc
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-04-22 11:37:16 -0500

    Added some submit functionality to the contact form. Now input text disappears after submitting. Also changed the cursor to hover on links and buttons. Also changed some setting on Email.js.

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,9 +1,10 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import emailjs from "@emailjs/browser"
 import { Form } from "react-router-dom"
 
 function Contact() {
     const form = useRef()
+    const [isSubmitted, setIsSubmitted] = useState(false)
 
     const sendEmail = (e) => {
         e.preventDefault()
@@ -15,6 +16,11 @@ function Contact() {
             .then(
                 () => {
                     console.log("SUCCESS!")
+                    setIsSubmitted(true)
+                    window.alert(
+                        `Your message has been sent!`
+                    )
+                    form.current.reset()
                 },
                 (error) => {
                     console.log("FAILED...", error.text)
@@ -28,19 +34,21 @@ function Contact() {
                 Here I think I want to have a contact form, with a text box where someone can write a message, and then send it, and it sends the message to Margot's email. I'll find out how to do that.
             </p>
             <p>
-                To reach me, you can email me at <a href="mailto:margotgoralczyk@gmail.com">margotgoralczyk@gmail.com</a> or call me at XXX-XXX-XXXX.
+                To reach me, you can email me at <a href="mailto:margot.goralczyk@gmail.com">margot.goralczyk@gmail.com</a> or call me at 412-398-4241.
             </p>
             <p>
-                You can also send me a message through this form:
+                You can also send me a message using this form:
             </p>
             <Form ref={form} onSubmit={sendEmail}>
-                <label>Name</label>
-                <input type="text" name="user_name" />
+                <label>First name</label>
+                <input type="text" name="first_name" />
+                <label>Last name</label>
+                <input type="text" name="last_name" />
                 <label>Email</label>
                 <input type="email" name="user_email" />
                 <label>Message</label>
                 <textarea name="message" />
-                <input type="submit" value="Send" />
+                <input type="submit" value="Send" className="button" />
             </Form>
         </div>
                 

34e1e58ba8b8b97e8fa5546d0e1557d0376b84ee
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-05-15 16:26:35 -0500

    Styled form.

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -39,29 +39,31 @@ function Contact() {
             <p>
                 You can also send me a message using this form:
             </p>
-            <div className="container">
-                <Form ref={form} onSubmit={sendEmail} action="" className="form">
-                    <div className="form-group">
-                        <label for="first_name" className="form-label">First name:</label>
-                        <input type="text" class="form-control" id="first_name" name="first_name" placeholder="e.g. Margot" tabIndex="1" required />
-                    </div>
-                    <div className="form-group">
-                        <label for="last_name">Last name:</label>
-                        <input type="text" class="form-control" id="last_name" name="last_name" placeholder="e.g. Goralczyk" tabIndex="2" required />
-                    </div>
-                    <div className="form-group">
-                        <label for="user_email">Email:</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="e.g. margot.goralczyk@gmail.com" tabIndex="3" required />
-                    </div>
-                    <div className="form-group">
-                        <label for="message">Message:</label>
-                        <textarea rows="5" cols="50" id="message" name="message" placeholder="Enter your message..." tabindex="4" />
-                    </div>
-                    <div>
-                        <button type="submit" value="Send" className="button">Send</button>
-                    </div>
-                    
-                </Form>
+            <div className="cont-container-form">
+                <div className="container-form">
+                    <Form ref={form} onSubmit={sendEmail} action="" className="form">
+                        <div className="form-group">
+                            <label for="first_name" className="form-label">First name:</label>
+                            <input type="text" className="form-control" id="first_name" name="first_name" placeholder="e.g. Margot" tabIndex="1" required />
+                        </div>
+                        <div className="form-group">
+                            <label for="last_name" className="form-label">Last name:</label>
+                            <input type="text" className="form-control" id="last_name" name="last_name" placeholder="e.g. Goralczyk" tabIndex="2" required />
+                        </div>
+                        <div className="form-group">
+                            <label for="user_email" className="form-label">Email:</label>
+                            <input type="email" className="form-control" id="email" name="email" placeholder="e.g. margot.goralczyk@gmail.com" tabIndex="3" required />
+                        </div>
+                        <div className="form-group">
+                            <label for="message" className="form-label">Message:</label>
+                            <textarea className="form-control" rows="5" cols="50" id="message" name="message" placeholder="Enter your message..." tabindex="4" />
+                        </div>
+                        <div>
+                            <button type="submit" value="Send" className="button">Send Message</button>
+                        </div>
+                        
+                    </Form>
+                </div>
             </div>
         </div>
                 

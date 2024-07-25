9b5b2ace1c0f360277027ec497f4d511bac677c7
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-07-25 13:01:08 -0500

    Cleaned up unused code and files.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import "./index.css";
-//import App from "./App";
-/*import { BrowserRouter as Router } from "react-router-dom";*/
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
@@ -37,16 +35,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-/*const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
-);
-*/
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
